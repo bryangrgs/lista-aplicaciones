7a3ac93c7ea7a449025b756f28f10ae42c5358ff
bryangrgs <103081416+bryangrgs@users.noreply.github.com>
2022-08-28 22:45:38 -0300

    aplicacion lista de tareas completada
    ultima actualizacion

--- src/components/ListaDeTareas.js
+++ src/components/ListaDeTareas.js
@@ -8,19 +8,43 @@ import Tarea from './Tarea';
 function ListaDeTareas(){
     
     const [tareas,setTareas]= useState([]);
+
     const agregarTarea = tarea =>{
-        console.log("tarea agregada");
+
         console.log(tarea);
+        
+        if (tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas =[tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
+    }
+    const eliminarTarea = id =>{
+        const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+        setTareas(tareasActualizadas);
+    }
+    const completarTarea = id =>{
+        const tareasActualizadas = tareas.map (tarea =>{
+            if (tarea.id === id){
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        });
+        setTareas(tareasActualizadas);
     }
     return(
         <>
-        <TareaFormulario/>
+        <TareaFormulario onSubmit={agregarTarea}/>
         <div className='tareas-lista-contenedor'>
             {
                 tareas.map((tarea)=>
                 <Tarea
+                key={tarea.id}
+                id={tarea.id}
                 texto={tarea.texto}
                 completada={tarea.completada}
+                completarTarea={completarTarea}
+                eliminarTarea={eliminarTarea}
                 />
                 )
             }
--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../stylesheet/Tarea.css'
 import {AiOutlineCloseCircle} from "react-icons/ai";
+
 function Tarea ({id,texto,completada,completarTarea,eliminarTarea}){
+    
     return(
         <div className={completada? 'tarea-contenedor completada': 'tarea-contenedor'}>
             <div className='tarea-texto' onClick={()=> completarTarea(id)}>
